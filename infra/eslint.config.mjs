0552bba2df52d3a44e82fd1e9ccac3a6c6ef83df
Gabriele Tonello <gabriele.tonello@mail.polimi.it>
2026-03-12 11:59:02 +0000

    Add ESLint for CDK TypeScript with @typescript-eslint and Prettier integration

---
+++ infra/eslint.config.mjs
@@ -0,0 +1,23 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['bin/**/*.ts', 'lib/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
